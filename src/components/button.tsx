222529f29dc88c38f337ed1747688533d0f670e5
FelipeCarrancho <f.carrancho@hotmail.com>
2025-02-22 02:44:57 -0300

    continuação aula 1 nlw

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,12 @@
-export function Button() {
-    return <button className="bg-violet-500 px-5 py-3 rounded-sm">botão</button>
+import { type ComponentProps, ReactNode } from 'react'
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900 "
+      {...props}
+    />
+  )
 }
